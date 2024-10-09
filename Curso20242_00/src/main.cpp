95460fde42ceadc84327ade09cedfc4d58024843
user <juan.restrepo06@uceva.edu.co>
2024-10-08 19:38:15 -0500

    actualizado con ssuma

--- Curso20242_00/src/main.cpp
+++ Curso20242_00/src/main.cpp
@@ -1,24 +1,18 @@
 #include <Arduino.h>
 
+// put function declarations here:
 int myFunction(int, int);
-void duplicar(int * puntero);
-int numero = 10; // variable global
-// @brief el punto de entrada del sisteme
 
 void setup() {
-  Serial.begin(115200);
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
 }
 
 void loop() {
-  Serial.println("numero = ");
-  Serial.println(numero);
-
-  duplicar(&numero);  // significa que le paso la direcciond e dmemoria de la var numero
-  delay(1000);
+  // put your main code here, to run repeatedly:
 }
 
-
-void duplicar (int * puntero){
-  * puntero = * puntero * 2;
-  Serial.println("[duplicar] numero= "+String(* puntero));
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
